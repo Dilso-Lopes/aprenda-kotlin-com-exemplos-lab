3567b5112feefc5bd35cb861025f0e5de8303da0
EDILSON LOPES DE ALMEIDA <105512422+Dilso-Lopes@users.noreply.github.com>
2023-01-09 16:03:25 -0300

    Concluído

--- desafio.kt
+++ desafio.kt
@@ -2,16 +2,17 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class Usuario(val nome: String)
+data class Usuario(var nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var nivel: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        println("$usuario")
+        inscritos.add(usuario)
+        println("$inscritos $conteudos")
         //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
@@ -19,9 +20,15 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 fun main() {
     var usuario = Usuario("Marcos")
     var conteudo: MutableList<ConteudoEducacional> = mutableListOf<ConteudoEducacional>()
-    conteudo.add(ConteudoEducacional("Linguas"))
-    var formacao = Formacao(usuario.nome, conteudo)
-    println("$formacao")
+  	var nivel = Nivel.BASICO
+    val nivel_selecionado = when (nivel){
+       Nivel.BASICO -> "Básico"
+       Nivel.INTERMEDIARIO -> "Intermediário"
+       Nivel.AVANCADO -> "Avançado"
+    }
+    conteudo.add(ConteudoEducacional("Inglês", nivel_selecionado))
+    val formacao = Formacao(conteudo)
+    formacao.matricular(usuario)
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
